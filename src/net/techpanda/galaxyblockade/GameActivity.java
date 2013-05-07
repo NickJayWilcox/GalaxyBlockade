28ef3135c2a632053830352316331563126da3e8
Nicholas Wilcox <wilcoxni@mail.gvsu.edu>
2013-05-07 02:05:09 -0400

    -Splash and Menu scenes -Resource and Scene managers

---
+++ src/net/techpanda/galaxyblockade/GameActivity.java
@@ -0,0 +1,75 @@
+package net.techpanda.galaxyblockade;
+
+import java.io.IOException;
+
+import net.techpanda.galaxyblockade.manager.ResourceManager;
+import net.techpanda.galaxyblockade.manager.SceneManager;
+
+import org.andengine.engine.Engine;
+import org.andengine.engine.LimitedFPSEngine;
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.handler.timer.ITimerCallback;
+import org.andengine.engine.handler.timer.TimerHandler;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.WakeLockOptions;
+import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
+//import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.BaseGameActivity;
+
+/**
+ * @author Nicholas Wilcox
+ * @version 1.0
+ */
+
+
+public class GameActivity extends BaseGameActivity
+{
+	//---------------------------------------------
+    // VARIABLES
+    //---------------------------------------------
+	private Camera camera;
+	private ResourceManager resourceManager;
+	
+	@Override
+	public Engine onCreateEngine(EngineOptions pEngineOptions) 
+	{
+	    return new LimitedFPSEngine(pEngineOptions, 60);
+	}
+	
+    public EngineOptions onCreateEngineOptions()
+    {
+    	camera = new Camera(0, 0, 480, 800);
+        EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.PORTRAIT_FIXED, new FillResolutionPolicy(), this.camera);
+        engineOptions.getAudioOptions().setNeedsMusic(true).setNeedsSound(true);
+        engineOptions.setWakeLockOptions(WakeLockOptions.SCREEN_ON);
+        return engineOptions;
+    }
+
+    public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback) throws IOException
+    {
+        ResourceManager.prepareManager(mEngine, this, camera, getVertexBufferObjectManager());
+        resourceManager = ResourceManager.getInstance();
+        pOnCreateResourcesCallback.onCreateResourcesFinished();
+    }
+
+    public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) throws IOException
+    {
+    	SceneManager.getInstance().createSplashScene(pOnCreateSceneCallback);
+    }
+
+    public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws IOException
+    {
+        mEngine.registerUpdateHandler(new TimerHandler(2f, new ITimerCallback() 
+        {
+            public void onTimePassed(final TimerHandler pTimerHandler) 
+            {
+                mEngine.unregisterUpdateHandler(pTimerHandler);
+                SceneManager.getInstance().createMenuScene();
+            }
+        }));
+        pOnPopulateSceneCallback.onPopulateSceneFinished();
+    }
+}
+
